ae4a806af8f20f9d1fc25df603c3897285a84209
Md Rayhan <mdrayhan9464@gmail.com>
2020-02-18 12:03:04 +0600

    dynamic name added

--- app/src/main/java/org/richit/graph/MainActivity.java
+++ app/src/main/java/org/richit/graph/MainActivity.java
@@ -3,14 +3,25 @@ package org.richit.graph;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.LegendEntry;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,14 +33,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
-        for (int i=0; i<10; i++){
-            numbers.add( new Entry( i, (float)i*5 ) );
+        for (float i=0; i<=480; i=i+10f){
+            numbers.add( new Entry( i, (float)(Math.sin( (double) i*3.1416/180 )+5) ) );
         }
+        //todo: lineDataSet
         LineDataSet lineDataSet = new LineDataSet( numbers, "Example" );
-        lineDataSet.setColor( R.color.colorAccent);
+        lineDataSet.setColor( Color.RED );
+        lineDataSet.setDrawFilled( true );
+        lineDataSet.setFillColor( Color.RED );
+
+        //todo: LineData
         LineData lineData = new LineData(lineDataSet);
+
+        //todo: LineChart
         LineChart chart = findViewById( R.id.chart );
         chart.setData(lineData);
         chart.invalidate();
+
+        //todo: XAxis
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setPosition( XAxis.XAxisPosition.BOTTOM);
+        xAxis.setTextSize(10f);
+        xAxis.setTextColor(Color.GRAY);
+//        xAxis.setDrawAxisLine(false);
+        xAxis.setDrawGridLines(false);
+        // set a custom value formatter
+        xAxis.setValueFormatter( new ValueFormatter() {
+            @Override
+            public String getAxisLabel(float value, AxisBase axis) {
+                return "Day "+(int)value;
+            }
+        } );
+
+        //todo: YAxis
+        YAxis left = chart.getAxisLeft();
+        left.setDrawLabels(false); // no axis labels
+        left.setDrawAxisLine(false); // no axis line
+        left.setDrawGridLines(false); // no grid lines
+        left.setDrawZeroLine(true); // draw a zero line
+        chart.getAxisRight().setEnabled(false); // no right axis
+
     }
 }
