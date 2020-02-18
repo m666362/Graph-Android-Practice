e8b9515e066f0227b1f89e8ddc51285b9e764461
Md Rayhan <mdrayhan9464@gmail.com>
2020-02-18 14:43:00 +0600

    added data shown in graph

--- app/src/main/java/org/richit/graph/MainActivity.java
+++ app/src/main/java/org/richit/graph/MainActivity.java
@@ -1,12 +1,18 @@
 package org.richit.graph;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.AxisBase;
@@ -28,30 +34,51 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<Entry> numbers = new ArrayList<Entry>(  );
+    Button addElementButton;
+    int count = 0;
+    LineDataSet lineDataSet;
+    LineData lineData;
+    LineChart chart;
+    XAxis xAxis;
+    YAxis left;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
-        for (float i=0; i<=480; i=i+10f){
-            numbers.add( new Entry( i, (float)(Math.sin( (double) i*3.1416/180 )+5) ) );
-        }
+
+        addElementButton = findViewById( R.id.addElement );
+        addElementButton.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                numbers.add( new Entry( (float) count, (float) Math.cos( (double) count*3.1416/180 ) ) );
+                setGraph();
+                count = count + 30;
+            }
+        } );
+
+
+        //Button
+
+    }
+
+    private void setGraph() {
         //todo: lineDataSet
-        LineDataSet lineDataSet = new LineDataSet( numbers, "Example" );
+        lineDataSet = new LineDataSet( numbers, "Example" );
         lineDataSet.setColor( Color.RED );
         lineDataSet.setDrawFilled( true );
         lineDataSet.setFillColor( Color.RED );
 
         //todo: LineData
-        LineData lineData = new LineData(lineDataSet);
+        lineData = new LineData(lineDataSet);
 
         //todo: LineChart
-        LineChart chart = findViewById( R.id.chart );
+        chart = findViewById( R.id.chart );
         chart.setData(lineData);
         chart.invalidate();
 
         //todo: XAxis
-        XAxis xAxis = chart.getXAxis();
+        xAxis = chart.getXAxis();
         xAxis.setPosition( XAxis.XAxisPosition.BOTTOM);
         xAxis.setTextSize(10f);
         xAxis.setTextColor(Color.GRAY);
@@ -66,12 +93,15 @@ public class MainActivity extends AppCompatActivity {
         } );
 
         //todo: YAxis
-        YAxis left = chart.getAxisLeft();
+        left = chart.getAxisLeft();
         left.setDrawLabels(false); // no axis labels
         left.setDrawAxisLine(false); // no axis line
         left.setDrawGridLines(false); // no grid lines
         left.setDrawZeroLine(true); // draw a zero line
         chart.getAxisRight().setEnabled(false); // no right axis
+    }
 
+    public void addElement(View view) {
+        Toast.makeText( this, ""+count++, Toast.LENGTH_SHORT ).show();
     }
 }
